35fde7d7eef962b6498a363d2dad6ce167221c1b
himanshu <himanshu@aethonlabs.com>
2025-07-20 01:21:53 -0700

    Initial Commit

---
+++ Pinned/PinnedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinnedApp.swift
+//  Pinned
+//
+//  Created by Himanshu on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PinnedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PinnedTests/PinnedTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinnedTests.swift
+//  PinnedTests
+//
+//  Created by Himanshu on 7/20/25.
+//
+
+import Testing
+@testable import Pinned
+
+struct PinnedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
